a3ac4cb6d3a9498a224da54c38a8920b01d02e34
Hung Vu <vunamhung@gmail.com>
2021-06-26 18:18:32 +0700

    update tailwind

--- postcss.config.js
+++ postcss.config.js
@@ -1,16 +1,11 @@
-const tailwindcss = require( 'tailwindcss' );
-const autoprefixer = require( 'autoprefixer' );
-const cssnano = require( 'cssnano' );
-const nested = require( 'postcss-nested' );
-
 const mode = process.env.NODE_ENV;
 const dev = mode === 'development';
 
 module.exports = {
 	plugins: [
-		tailwindcss,
-		nested,
-		autoprefixer,
-		! dev && cssnano( { preset: 'default' } ),
+		require( 'postcss-import' ),
+		require( 'tailwindcss/nesting' ),
+		require( 'tailwindcss' ),
+		! dev && require( 'cssnano' )( { preset: 'default' } ),
 	],
 };
